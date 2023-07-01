e202a58d6c0b19e1c3e0fb88bd72dee26a32fc8c
Badmus Sulaimon <72830725+haryobamy@users.noreply.github.com>
2023-07-01 21:55:39 +0100

    Update config.ts

--- src/constant/https/config.ts
+++ src/constant/https/config.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://chatter-backend-qg9iow906-haryobamy.vercel.app/;
+const BASE_URL = "https://chatter-backend-qg9iow906-haryobamy.vercel.app/";
 const DB_USER = "chatteruser";
 
 export { BASE_URL, DB_USER };
